8da4857a40189915e8ef89577450f9b8b956b411
Mattia Cacciatore <mattiacacciatore.mc@gmail.com>
2025-10-10 15:01:52 +0200

    [inputs] [ball-shoot] feat: Refatored PlayerInputProvider to detect input from mouse/touch instead of the placeholder space bar. Coroutine-based logic moved to main Update loop. Touch controls tested with Editor's simulation only.

--- Assets/Scripts/PlayerInputProvider.cs
+++ Assets/Scripts/PlayerInputProvider.cs
@@ -1,13 +1,16 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
-// #TODO: Rethink class naming and architecture.
+// #TODO: Rethink class naming.
 public class PlayerInputProvider : MonoBehaviour
 {
     [SerializeField] private BasketballPlayer player;
     [SerializeField] private float maxTimeFrame = 2f;
+
+    [Header("Input Actions")]
+    [SerializeField] private InputActionReference pointerAction;
+    [SerializeField] private InputActionReference pressAction;
 
     [Header("Events")]
     public UnityEvent onInputStarted;
@@ -19,49 +22,69 @@ public class PlayerInputProvider : MonoBehaviour
 
     public float CurrentValue => Mathf.Sqrt(_inputValue);
 
-    private void Update()
+
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !_pressed)
-        {
-            _pressed = true;
-            StartCoroutine(ReadInputCoroutine());
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _pressed = false;
-        }
+        pressAction.action.performed += OnPressActionPerformed;
+        pressAction.action.canceled += OnPressActionCanceled;
     }
 
-
-    private IEnumerator ReadInputCoroutine()
+    private void Update()
     {
-        onInputStarted.Invoke();
+        if (!_pressed)
+            return;
+
         /*
          *  #NOTE: Where it's mathematically possible, magnitudes are kept squared to avoid
          *  repeated square root computations.
          */
+
         var min = player.MinShotVelocity.sqrMagnitude;
         var max = player.MaxShotVelocity.sqrMagnitude;
 
-        _inputValue = player.MinShotVelocity.sqrMagnitude;
+        var normalizedPointerDeltaY = Mathf.Clamp01(pointerAction.action.ReadValue<Vector2>().y / Screen.height);
+        _inputValue = Mathf.Clamp(
+                _inputValue + normalizedPointerDeltaY * (max - min),
+                min,
+                max);
+
+        Debug.Log($"[{GetType().Name}] Input value: {_inputValue}");
+
+        _timeElapsed += Time.deltaTime;
+
+        if(_timeElapsed > maxTimeFrame)
+            SendInput();
+    }
+
+    private void OnPressActionPerformed(InputAction.CallbackContext context)
+    {
         _timeElapsed = 0f;
-        while (_pressed && _timeElapsed <= maxTimeFrame)
+        _inputValue = player.MinShotVelocity.sqrMagnitude;
+
+        _pressed = true;
+        onInputStarted.Invoke();
+    }
+
+    private void OnPressActionCanceled(InputAction.CallbackContext context)
+    {
+        if (_pressed)
         {
-            var speed = (max - min) / maxTimeFrame;
-            _inputValue = Mathf.Clamp(_inputValue + speed * Time.deltaTime, min, max);
-            Debug.Log($"[{GetType().Name}] Input value: {_inputValue}");
-
-            _timeElapsed += Time.deltaTime;
-            yield return null;
+            SendInput();
         }
-
-        SendInput();
     }
 
     private void SendInput()
     {
         onInputPerformed.Invoke(CurrentValue);
+
+        _inputValue = player.MinShotVelocity.sqrMagnitude;
         _pressed = false;
+    }
+
+    private void OnDisable()
+    {
+        pressAction.action.performed -= OnPressActionPerformed;
+        pressAction.action.canceled -= OnPressActionCanceled;
     }
 
     private void OnDestroy()
